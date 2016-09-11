ecd11e4b0056234b7677a6b885ee1ee7373cd5dd
Thomas Lo <thomasjinlo@gmail.com>
2016-09-10 22:18:42 -0700

    Change App to a pure component in components directory

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Digital from './digital/container'
+// import Calendar from './components/calendar/container'
+
+export default () => {
+  return (
+    <div>
+      <Digital />
+    </div>
+  )
+};
