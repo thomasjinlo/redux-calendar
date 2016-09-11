aae5254a111f29193327aa89ead1cf5d6194e2a1
Thomas Lo <thomasjinlo@gmail.com>
2016-09-11 00:30:26 -0700

    Renders empty squares and squares in a month

---
+++ src/components/calendar/daysOfMonth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const startDay = date => new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+const numDaysInMonth = date => new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+const renderEmptySquares = (startDay, collection=[]) => {
+  if (startDay === 0) return;
+
+  collection.push(<div key={startDay} className="weekday"></div>);
+  renderEmptySquares((startDay - 1), collection)
+
+  return collection.map( div => div);
+};
+
+export default props => {
+  return (
+    <div>
+      { renderEmptySquares(startDay(props.date)) }
+      { numDaysInMonth }
+    </div>
+  )
+}
