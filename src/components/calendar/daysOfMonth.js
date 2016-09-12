d248f418232d3ae2256984ea0afcc8495db5d9a3
Thomas Lo <thomasjinlo@gmail.com>
2016-09-12 11:29:08 -0700

    Render squares

--- src/components/calendar/daysOfMonth.js
+++ src/components/calendar/daysOfMonth.js
@@ -14,11 +14,20 @@ const renderEmptySquares = (startDay, collection=[]) => {
   return collection.map( div => div);
 };
 
+const renderSquares = (daysInMonth, collection=[]) => {
+  if (daysInMonth === 0) return;
+
+  collection.unshift(<div key={daysInMonth} id={daysInMonth} className="weekday">{daysInMonth}</div>);
+  renderSquares((daysInMonth - 1), collection);
+
+  return collection.map( div => div);
+}
+
 export default props => {
   return (
     <div>
       { renderEmptySquares(startDay(props.date)) }
-      { numDaysInMonth }
+      { renderSquares(numDaysInMonth(props.date)) }
     </div>
   )
 }
