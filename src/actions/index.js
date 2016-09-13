3455ee6bc68fa3a1865e5f91880f3d7d0bcf0fad
Thomas Lo <thomasjinlo@gmail.com>
2016-09-12 17:49:44 -0700

    Changes date to selected date

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
 export function setDateToday () {
   let date = new Date();
-  
+
   return {
     type: 'SET_DATE_TODAY',
     payload: date,
+  }
+}
+
+export function changeDateInMonth (date, selectedDay) {
+  let newDate = new Date(date.getFullYear(), date.getMonth(), selectedDay);
+  console.log(newDate)
+  return {
+    type: 'SET_SELECTED_DATE',
+    payload: newDate,
   }
 }
