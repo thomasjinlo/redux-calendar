6a6a098cf3adbe9d5387112012b81917f4ca76c8
Thomas Lo <thomasjinlo@gmail.com>
2016-09-12 23:34:27 -0700

    Add active class if id matches activeID

--- src/components/calendar/square.js
+++ src/components/calendar/square.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 import './calendar.css';
 
+const activeDate = (id, activeID) => {
+  console.log(id, activeID)
+  return id === activeID
+}
+
 export default props => {
   return (
-    <div id={props.dayInMonth} className="squares" onClick={() => props.changeDateInMonth(props.date, props.dayInMonth)}>{props.dayInMonth}</div>
+    <div id={props.dayInMonth} className={`squares ${activeDate(props.dayInMonth, props.activeID) ? "active" : ""}`}
+                               onClick={() => {
+                                 console.log(props)
+                                 props.changeDateInMonth(props.date, props.dayInMonth)
+                                 props.setActiveToday(props.dayInMonth)
+                               }}>
+    {props.dayInMonth}</div>
   )
 }
