faabe438fa26840a2594f3a3172a61ce398eac92
Thomas Lo <thomasjinlo@gmail.com>
2016-09-12 23:35:10 -0700

    Move action creator from Digital and bring it into calendar container

--- src/components/digital/container.js
+++ src/components/digital/container.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
-import { setDateToday } from '../../actions'
 
 import WordDay from './wordDay'
 import NumDay from './numDay'
 
 class Digital extends Component {
-  componentDidMount() {
-    this.props.setDateToday();
-  };
-
   render() {
     return (
       <div className="digitalContainer">
@@ -22,16 +15,10 @@ class Digital extends Component {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    setDateToday: setDateToday,
-  }, dispatch)
-}
-
 function mapStateToProps(state) {
   return {
     date: state.date,
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Digital);
+export default connect(mapStateToProps)(Digital);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import dateReducer from './dateReducer';
+import selectedDateReducer from './selectedDateReducer';
 
 const rootReducer = combineReducers({
   date: dateReducer,
+  activeID: selectedDateReducer,
 });
 
 export default rootReducer;
