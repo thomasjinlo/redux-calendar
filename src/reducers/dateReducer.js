8a463ce4b02e3668b7352b9564cbe4feea59c993
Thomas Lo <thomasjinlo@gmail.com>
2016-09-12 17:51:40 -0700

    Return new selected date

--- src/reducers/dateReducer.js
+++ src/reducers/dateReducer.js
@@ -2,6 +2,8 @@ export default function (state = new Date(), action) {
   switch (action.type) {
     case 'SET_DATE_TODAY':
       return action.payload
+    case 'SET_SELECTED_DATE':
+      return action.payload
     default:
       return state
   }
